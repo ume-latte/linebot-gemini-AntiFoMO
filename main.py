cb4ac65b7a28b17266cabdc0f1b5e0da8816926e
b-sugar LaTTe <117913258+ume-latte@users.noreply.github.com>
2024-07-07 10:29:26 +0800

    keyword3.py

--- main.py
+++ main.py
@@ -1,8 +1,54 @@
+import logging
+import os
+import re
+import sys
+from datetime import datetime
+import requests
+
+from fastapi import FastAPI, HTTPException, Request
+from linebot import (
+    LineBotApi, WebhookParser
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage
+)
+import uvicorn
+
+import google.generativeai as genai
+from firebase import firebase
+from utils import check_image_quake, check_location_in_message, get_current_weather, get_weather_data, simplify_data
+
+app = FastAPI()
+logging.basicConfig(level=logging.INFO)  # 設置適當的日誌級別
+logger = logging.getLogger(__name__)
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET')
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+if not channel_secret or not channel_access_token:
+    logger.error('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
+
+firebase_url = os.getenv('FIREBASE_URL')
+gemini_key = os.getenv('GEMINI_API_KEY')
+
+# 初始化 Gemini Pro API
+genai.configure(api_key=gemini_key)
+
+@app.get("/health")
+async def health():
+    return 'ok'
+
 @app.post("/webhooks/line")
 async def handle_callback(request: Request):
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers.get('X-Line-Signature')
 
-    # get request body as text
+    # 取得請求主體的文字
     body = await request.body()
     body = body.decode()
 
@@ -12,7 +58,6 @@ async def handle_callback(request: Request):
         raise HTTPException(status_code=400, detail="Invalid signature")
 
     for event in events:
-        logging.info(event)
         if not isinstance(event, MessageEvent):
             continue
         if not isinstance(event.message, TextMessage):
@@ -23,7 +68,7 @@ async def handle_callback(request: Request):
         # 關鍵字過濾
         ignore_keywords = ["什麼是FoMO", "緩解FoMO指南", "FoMO測試", "連接spotify", "推薦歌曲", "推薦播放清單"]
         if any(keyword in text for keyword in ignore_keywords):
-            return 'OK'  # Ignore the message if it contains any of the ignore_keywords
+            return 'OK'  # 忽略包含任何 ignore_keywords 的訊息
 
         msg_type = event.message.type
         fdb = firebase.FirebaseApplication(firebase_url, None)
@@ -53,56 +98,66 @@ async def handle_callback(request: Request):
                 "其他": 'I'
             }
 
-            model = genai.GenerativeModel('gemini-1.5-pro')
-            response = model.generate_content(
-                f'請判斷 {text} 裡面的文字屬於 {bot_condition} 裡面的哪一項？符合條件請回傳對應的英文文字就好，不要有其他的文字與字元。'
-            )
-            text_condition = re.sub(r'[^A-Za-z]', '', response.text.strip())
-            reply_msg = ""
-
-            if text_condition == 'A':
-                fdb.delete(user_chat_path, None)
-                reply_msg = '已清空對話紀錄'
-            elif text_condition == 'B':
-                model = genai.GenerativeModel('gemini-pro')
+            try:
+                model = genai.GenerativeModel('gemini-1.5-pro')
                 response = model.generate_content(
-                    f'Summary the following message in Traditional Chinese by less 5 list points. \n{messages}'
+                    f'請判斷 {text} 裡面的文字屬於 {bot_condition} 裡面的哪一項？符合條件請回傳對應的英文文字就好，不要有其他的文字與字元。'
                 )
-                reply_msg = response.text
-            elif text_condition == 'C':
-                model = genai.GenerativeModel('gemini-pro-vision')
-                OPEN_API_KEY = os.getenv('OPEN_API_KEY')
-                earth_res = requests.get(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/E-A0015-003?Authorization={OPEN_API_KEY}&downloadType=WEB&format=JSON')
-                url = earth_res.json()["cwaopendata"]["Dataset"]["Resource"]["ProductURL"]
-                reply_msg = check_image_quake(url) + f'\n\n{url}'
-            elif text_condition in ['E', 'F', 'G', 'H']:
-                reply_msg = '如下'
-            elif text_condition == 'I':
-                location_text = '台北市'
-                location = check_location_in_message(location_text)
-                weather_data = get_weather_data(location)
-                simplified_data = simplify_data(weather_data)
-                current_weather = get_current_weather(simplified_data)
+                text_condition = re.sub(r'[^A-Za-z]', '', response.text.strip())
+                reply_msg = ""
 
-                now = datetime.now()
-                formatted_time = now.strftime("%Y/%m/%d %H:%M:%S")
-
-                if current_weather is not None:
-                    total_info = f'位置: {location}\n氣候: {current_weather["Wx"]}\n降雨機率: {current_weather["PoP"]}\n體感: {current_weather["CI"]}\n現在時間: {formatted_time}'
+                if text_condition == 'A':
+                    fdb.delete(user_chat_path, None)
+                    reply_msg = '已清空對話紀錄'
+                elif text_condition == 'B':
+                    model = genai.GenerativeModel('gemini-pro')
                     response = model.generate_content(
-                        f'請用繁體中文、以精簡並且不要加上任何文字格式（包括粗體斜體還有*號等等）回覆以下的訊息，{text}'
+                        f'Summary the following message in Traditional Chinese by less 5 list points. \n{messages}'
                     )
                     reply_msg = response.text
-            else:
-                messages.append({'role': 'user', 'parts': [text]})
-                response = model.generate_content(messages)
-                messages.append({'role': 'model', 'parts': [text]})
-                fdb.put_async(user_chat_path, None, messages)
-                reply_msg = response.text
+                elif text_condition == 'C':
+                    model = genai.GenerativeModel('gemini-pro-vision')
+                    OPEN_API_KEY = os.getenv('OPEN_API_KEY')
+                    earth_res = requests.get(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/E-A0015-003?Authorization={OPEN_API_KEY}&downloadType=WEB&format=JSON')
+                    url = earth_res.json()["cwaopendata"]["Dataset"]["Resource"]["ProductURL"]
+                    reply_msg = check_image_quake(url) + f'\n\n{url}'
+                elif text_condition in ['E', 'F', 'G', 'H']:
+                    reply_msg = '如下'
+                elif text_condition == 'I':
+                    location_text = '台北市'
+                    location = check_location_in_message(location_text)
+                    weather_data = get_weather_data(location)
+                    simplified_data = simplify_data(weather_data)
+                    current_weather = get_current_weather(simplified_data)
 
-            await line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=reply_msg)
-            )
+                    now = datetime.now()
+                    formatted_time = now.strftime("%Y/%m/%d %H:%M:%S")
+
+                    if current_weather is not None:
+                        total_info = f'位置: {location}\n氣候: {current_weather["Wx"]}\n降雨機率: {current_weather["PoP"]}\n體感: {current_weather["CI"]}\n現在時間: {formatted_time}'
+                        response = model.generate_content(
+                            f'請用繁體中文、以精簡並且不要加上任何文字格式（包括粗體斜體還有*號等等）回覆以下的訊息，{text}'
+                        )
+                        reply_msg = response.text
+                else:
+                    messages.append({'role': 'user', 'parts': [text]})
+                    response = model.generate_content(messages)
+                    messages.append({'role': 'model', 'parts': [text]})
+                    fdb.put_async(user_chat_path, None, messages)
+                    reply_msg = response.text
+
+                await line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=reply_msg)
+                )
+
+            except Exception as e:
+                logger.error(f"Error processing message: {e}")
 
     return 'OK'
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    debug = os.environ.get('API_ENV', 'develop') == 'develop'
+    logging.info('Application will start...')
+    uvicorn.run("main:app", host="0.0.0.0", port=port, debug=debug)
